92dd6a308a3485e7673457928cc424943f7cbd97
Halloween21 <simon@alacarte-kurier.de>
2022-01-07 13:32:31 +0100

    resolved confilct

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main{
     public static void main (String [] args){
         System.out.println("Hello World");
-<<<<<<< HEAD
-        System.out.println("This line is only in Branch_1");
-=======
         System.out.println("This code is only in master");
->>>>>>> master
     }
 }
