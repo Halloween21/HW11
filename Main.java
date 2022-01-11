f50445610a9f626aacd73b91f563c5cabc5a048c
Halloween21 <simon@alacarte-kurier.de>
2022-01-11 17:35:45 +0100

    added faculty class in main

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main{
     public static void main (String [] args){
-        System.out.println("Hello World");
-        System.out.println("This code is only in master");
+        int a=5;
+        System.out.println(fak(5));
+    }
+
+    public static int fak(int i) {
+        if (i==0 || i==1){
+            return 1;
+        }
+        else {
+            return fak(i-1) + fak (i-2);
+        }
     }
 }
